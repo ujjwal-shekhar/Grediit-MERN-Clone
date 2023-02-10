cd8a5530d8ba7c6c65f35628aab6236a3816b98c
Ujjwal Shekhar <ujjwal.shekhar@research.iiit.ac.in>
2023-02-10 19:44:40 +0530

    Post Card underway TODO: - Backend using postcard

---
+++ client/src/Components/Posts/Comments.jsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import Avatar from '@mui/material/Avatar';
+import CardHeader from '@mui/material/CardHeader';
+import AddCommentIcon from '@mui/icons-material/AddComment';
+import IconButton from '@mui/material/IconButton';
+
+export default function Comment() {
+  return (
+    <Card sx={{ maxWidth: 500 }}>
+        <CardHeader
+            avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                R
+            </Avatar>
+            }
+            // action={
+            // <IconButton aria-label="settings">
+            //     <AddCommentIcon />
+            // </IconButton>
+            // }
+            title="Shrimp and Chorizo Paella"
+            subheader="September 14, 2016"
+        />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      {/* <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+    </Card>
+  );
+}
