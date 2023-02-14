a2f864b2e00a5165e5f7c737880aa82cd4b0dd79
Ujjwal Shekhar <ujjwal.shekhar@research.iiit.ac.in>
2023-02-14 22:35:52 +0530

    Making my SGs

--- client/src/Components/Subgreddiits/MiniSubgreddiitCard.jsx
+++ client/src/Components/Subgreddiits/MiniSubgreddiitCard.jsx
@@ -1,89 +1,113 @@
-import React from 'react';
-import {
-  MDBCol,
-  MDBContainer,
-  MDBRow,
-  MDBCard,
-  MDBCardText,
-  MDBCardBody,
-  MDBCardImage,
-  MDBBtn,
-  MDBTypography,
-  MDBIcon
-} from 'mdb-react-ui-kit';
+import { Card, Space } from 'antd';
+import Button from 'antd/lib/button';
 
-export default function MiniSubgreddiitCard() {
+const App = ({ subgreddiit, perms }) => {
+  const handleClick = () => {
+    console.log('Clicked');
+  }
   return (
-    <section className="vh-100" style={{ backgroundColor: '#5f59f7' }}>
-      <MDBContainer className="py-5 h-100">
-        <MDBRow className="justify-content-center align-items-center h-100">
-          <MDBCol xl="10">
-            <MDBCard className="mb-5" style={{ borderRadius: '15px' }}>
-              <MDBCardBody className="p-4">
-                <MDBTypography tag='h3'>Program Title</MDBTypography>
-                <MDBCardText className="small">
-                  <MDBIcon far icon="star" size="lg" />
-                  <span className="mx-2">|</span> Created by <strong>MDBootstrap</strong> on 11 April , 2021
-                </MDBCardText>
-                <hr className="my-4" />
-                <div className="d-flex justify-content-start align-items-center">
-                  <MDBCardText className="text-uppercase mb-0">
-                    <MDBIcon fas icon="cog me-2" /> <span className="text-muted small">settings</span>
-                  </MDBCardText>
-                  <MDBCardText className="text-uppercase mb-0">
-                    <MDBIcon fas icon="link ms-4 me-2" /> <span className="text-muted small">program link</span>
-                  </MDBCardText>
-                  <MDBCardText className="text-uppercase mb-0">
-                    <MDBIcon fas icon="ellipsis-h ms-4 me-2" /> <span className="text-muted small">program link</span> <span className="ms-3 me-4">|</span>
-                  </MDBCardText>
-                  <a href="#!">
-                    <MDBCardImage
-                      width="35"
-                      src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp"
-                      alt="avatar"
-                      className="rounded-circle me-3"
-                      fluid />
-                  </a>
-                  <MDBBtn outline color="dark" floating size="sm">
-                    <MDBIcon fas icon="plus" />
-                  </MDBBtn>
-                </div>
-              </MDBCardBody>
-            </MDBCard>
-
-            <MDBCard className="mb-5" style={{ borderRadius: '15px' }}>
-              <MDBCardBody className="p-4">
-                <MDBTypography tag='h3'>Company Culture</MDBTypography>
-                <MDBCardText className="small">
-                  <MDBIcon fas icon="star text-warning" size="lg" />
-                  <span className="mx-2">|</span> Public <span className="mx-2">|</span> Updated by <strong>MDBootstrap</strong> on 11 April , 2021
-                </MDBCardText>
-                <hr className="my-4" />
-                <div className="d-flex justify-content-start align-items-center">
-                  <MDBCardText className="text-uppercase mb-0">
-                    <MDBIcon fas icon="cog me-2" /> <span className="text-muted small">settings</span>
-                  </MDBCardText>
-                  <MDBCardText className="text-uppercase mb-0">
-                    <MDBIcon fas icon="link ms-4 me-2" /> <span className="text-muted small">program link</span>
-                  </MDBCardText>
-                  <MDBCardText className="text-uppercase mb-0">
-                    <MDBIcon fas icon="ellipsis-h ms-4 me-2" /> <span className="text-muted small">program link</span> <span className="ms-3 me-4">|</span>
-                  </MDBCardText>
-                  <a href="#!">
-                    <MDBCardImage width="35" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp" alt="avatar" className="rounded-circle me-1" fluid />
-                    <MDBCardImage width="35" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-3.webp" alt="avatar" className="rounded-circle me-1" fluid />
-                    <MDBCardImage width="35" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-4.webp" alt="avatar" className="rounded-circle me-1" fluid />
-                    <MDBCardImage width="35" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar" className="rounded-circle me-3" fluid />
-                  </a>
-                  <MDBBtn outline color="dark" floating size="sm">
-                    <MDBIcon fas icon="plus" />
-                  </MDBBtn>
-                </div>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    </section>
+    <Space direction="vertical" size={16}>
+      <Card
+        title={subgreddiit.name}
+        extra={perms == "MOD" ? <Button type="primary" danger onClick={handleClick}>Delete</Button> : null}
+        style={{
+          width: 300,
+        }}
+      >
+        <p>Description : {subgreddiit.description}</p>
+        <p>Number of People : {subgreddiit.moderators.length + subgreddiit.common_members.length}</p>
+        <p>Number of posts : {subgreddiit.posts.length}</p>
+      </Card>
+    </Space>
   );
-}
+};
+export default App;
+// import React from 'react';
+// import {
+//   MDBCol,
+//   MDBContainer,
+//   MDBRow,
+//   MDBCard,
+//   MDBCardText,
+//   MDBCardBody,
+//   MDBCardImage,
+//   MDBBtn,
+//   MDBTypography,
+//   MDBIcon
+// } from 'mdb-react-ui-kit';
+
+// export default function MiniSubgreddiitCard({ subgreddiit, perms }) {
+//   return (
+//     <section className="" style={{ backgroundColor: '#5f59f7' }}>
+//       <MDBContainer className="py-5 h-100">
+//         <MDBRow className="justify-content-center align-items-center h-100">
+//           <MDBCol xl="10">
+//             <MDBCard className="mb-1" style={{ borderRadius: '15px' }}>
+//               <MDBCardBody className="p-4">
+//                 <MDBTypography tag='h3'>{subgreddiit.name}</MDBTypography>
+//                 <MDBCardText className="small">
+//                   <MDBIcon far icon="star" size="lg" />
+//                   <span className="mx-2">|</span> Created by <strong>{subgreddiit.moderators[0]}</strong> on 11 April , 2021
+//                 </MDBCardText>
+//                 <hr className="my-4" />
+//                 <div className="d-flex justify-content-start align-items-center">
+//                   <MDBCardText className="text-uppercase mb-0">
+//                     <MDBIcon fas icon="cog me-2" /> <span className="text-muted small">settings</span>
+//                   </MDBCardText>
+//                   <MDBCardText className="text-uppercase mb-0">
+//                     <MDBIcon fas icon="link ms-4 me-2" /> <span className="text-muted small">program link</span>
+//                   </MDBCardText>
+//                   <MDBCardText className="text-uppercase mb-0">
+//                     <MDBIcon fas icon="ellipsis-h ms-4 me-2" /> <span className="text-muted small">program link</span> <span className="ms-3 me-4">|</span>
+//                   </MDBCardText>
+//                   <a href="#!">
+//                     <MDBCardImage
+//                       width="35"
+//                       src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp"
+//                       alt="avatar"
+//                       className="rounded-circle me-3"
+//                       fluid />
+//                   </a>
+//                   <MDBBtn outline color="dark" floating size="sm">
+//                     <MDBIcon fas icon="plus" />
+//                   </MDBBtn>
+//                 </div>
+//               </MDBCardBody>
+//             </MDBCard>
+
+//             {/* <MDBCard className="mb-5" style={{ borderRadius: '15px' }}>
+//               <MDBCardBody className="p-4">
+//                 <MDBTypography tag='h3'>Company Culture</MDBTypography>
+//                 <MDBCardText className="small">
+//                   <MDBIcon fas icon="star text-warning" size="lg" />
+//                   <span className="mx-2">|</span> Public <span className="mx-2">|</span> Updated by <strong>MDBootstrap</strong> on 11 April , 2021
+//                 </MDBCardText>
+//                 <hr className="my-4" />
+//                 <div className="d-flex justify-content-start align-items-center">
+//                   <MDBCardText className="text-uppercase mb-0">
+//                     <MDBIcon fas icon="cog me-2" /> <span className="text-muted small">settings</span>
+//                   </MDBCardText>
+//                   <MDBCardText className="text-uppercase mb-0">
+//                     <MDBIcon fas icon="link ms-4 me-2" /> <span className="text-muted small">program link</span>
+//                   </MDBCardText>
+//                   <MDBCardText className="text-uppercase mb-0">
+//                     <MDBIcon fas icon="ellipsis-h ms-4 me-2" /> <span className="text-muted small">program link</span> <span className="ms-3 me-4">|</span>
+//                   </MDBCardText>
+//                   <a href="#!">
+//                     <MDBCardImage width="35" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp" alt="avatar" className="rounded-circle me-1" fluid />
+//                     <MDBCardImage width="35" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-3.webp" alt="avatar" className="rounded-circle me-1" fluid />
+//                     <MDBCardImage width="35" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-4.webp" alt="avatar" className="rounded-circle me-1" fluid />
+//                     <MDBCardImage width="35" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar" className="rounded-circle me-3" fluid />
+//                   </a>
+//                   <MDBBtn outline color="dark" floating size="sm">
+//                     <MDBIcon fas icon="plus" />
+//                   </MDBBtn>
+//                 </div>
+//               </MDBCardBody>
+//             </MDBCard> */}
+//           </MDBCol>
+//         </MDBRow>
+//       </MDBContainer>
+//     </section>
+//   );
+// }
--- client/src/Components/pages/UserSubgreddiits.jsx
+++ client/src/Components/pages/UserSubgreddiits.jsx
@@ -1,11 +1,42 @@
 import MiniSubgreddiitCard from '../Subgreddiits/MiniSubgreddiitCard.jsx'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import Item from '@mui/material/Stack';
 
 export default function UserSubgreddiits() {
+    const subgreddiit = {
+        name: 'test',
+        description: 'LOREM IPSUM DOLOR SIT AMET',
+        moderators: ['test'],
+        banned_keywords: ['test'],
+        posts : ['test1', 'test2', 'test3'],
+        common_members: ['test1', 'test2', 'test3'],
+    }
+    const handleAddSG = () => {
+        console.log("Add SG")
+    }
     return (
         <div>
-        <MiniSubgreddiitCard />
-        <MiniSubgreddiitCard />
-        <MiniSubgreddiitCard />
+        <Stack>
+            <Item>
+                <MiniSubgreddiitCard subgreddiit={subgreddiit} perms={"MOD"}/>
+            </Item>
+            <Item>
+                <MiniSubgreddiitCard subgreddiit={subgreddiit} perms={"MOD"}/>
+            </Item>
+        </Stack>
+        <IconButton onClick={handleAddSG} sx={{
+            position: 'absolute',
+            bottom: 0,
+            right: 0,
+            // color: 'white',
+            // backgroundColor: 'primary',
+        }}>
+            <AddCircleOutlineIcon fontSize='large' color='primary'/>
+        </IconButton>
+        {/* <MiniSubgreddiitCard />
+        <MiniSubgreddiitCard /> */}
         </div>
     )
 }
